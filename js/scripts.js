900cc7eeb434c6cca79df554d0327d774b9df50b
Gareth Grindeland <gt.grindeland@gmail.com>
2023-03-22 09:49:42 -0700

    Add console log to check for bug in verb code. Bugfix: typo in innerText.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,31 @@
 window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
-    // in this section we get the value for each form input
+    event.preventDefault();
     const person1Input = document.getElementById("person1Input").value;
     const person2Input = document.getElementById("person2Input").value;
     const animalInput= document.getElementById("animalInput").value;
     const exclamationInput = document.getElementById("exclamationInput").value;
     const verbInput = document.getElementById("verbInput").value;
+    // Here's the first log we added.
+    console.log("verbInput = " + verbInput);
     const nounInput = document.getElementById("nounInput").value;
 
-    // then we set the story variables to the values we got from the form
     document.querySelector("span#person1a").innerText = person1Input;
     document.querySelector("span#person1b").innerText = person1Input;
     document.querySelector("span#person1c").innerText = person1Input;
     document.querySelector("span#person2a").innerText = person2Input;
     document.querySelector("span#person2b").innerText = person2Input;
     document.querySelector("span#animal").innerText = animalInput;
+    // Here are the 3 new logs!
+    console.log("Correctly targeting <span>? = ", document.querySelector("span#verb"));
+    console.log("Correctly targeting innerText? = ", document.querySelector("span#verb").inerText);
     document.querySelector("span#verb").innerText = verbInput;
+    console.log("Correctly referencing verbInput and assigning value of innerText? = ", document.querySelector("span#verb").inerText);
+    // Above are the 3 new logs!
     document.querySelector("span#noun").innerText = nounInput;
     document.querySelector("span#exclamation").innerText = exclamationInput;
 
     document.querySelector("div#story").removeAttribute("class");
-
-    event.preventDefault();
   };
 };
